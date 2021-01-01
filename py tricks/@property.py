a4a65d198c19579267b5fecac48c0b8fcbc82a22
Sharan <michinaux.marko@gmail.com>
2021-01-01 13:27:53 -0400

    Create @property.py

---
+++ py tricks/@property.py
@@ -0,0 +1,31 @@
+#For this, we reuse the temperature name while defining our getter and setter functions. Let's look at how to implement this as a decorator:
+
+# Using @property decorator
+class Celsius:
+    def __init__(self, temperature=0):
+        self.temperature = temperature
+
+    def to_fahrenheit(self):
+        return (self.temperature * 1.8) + 32
+
+    @property
+    def temperature(self):
+        print("Getting value...")
+        return self._temperature
+
+    @temperature.setter
+    def temperature(self, value):
+        print("Setting value...")
+        if value < -273.15:
+            raise ValueError("Temperature below -273 is not possible")
+        self._temperature = value
+
+
+# create an object
+human = Celsius(37)
+
+print(human.temperature)
+
+print(human.to_fahrenheit())
+
+coldest_thing = Celsius(-300)
